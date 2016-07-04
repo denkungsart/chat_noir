0677701efc88c18c583dc5c22cbb734379c4bf5e
Evgeny Sugakov <shiroginne@gmail.com>
2016-07-04 17:15:18 +0200

    Change VCR settings & update cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,11 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+
+  c.default_cassette_options = {
+    match_requests_on: [:uri, :path],
+    record: :new_episodes
+  }
 end
 
 RSpec.configure do |config|
